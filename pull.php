2e7380be3668149c1efdffd5cb6f3a2c1b320c4e
Eugene Lugin <call.eugene@gmail.com>
2015-04-13 10:19:13 +0300

    Pull on github webhoook

--- pull.php
+++ pull.php
@@ -1,9 +1,18 @@
 <?php
 define('PRIVATE_KEY', 'q1w2e3r4t5y6u7i8o9p0');
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST'
-        && $_REQUEST['thing'] === PRIVATE_KEY)
+if ($_SERVER['REQUEST_METHOD'] === 'POST')
 {
+    $headers = getallheaders();
+    $hubSignature = $headers['X-Hub-Signature'];
+    list($algo, $hash) = explode('=', $hubSignature, 2);
+    $payload = file_get_contents('php://input');
+    $payloadHash = hash_hmac($algo, $payload, PRIVATE_KEY);
+    if ($hash !== $payloadHash) {
+        die('Bad secret');
+    }
+    $data = json_decode($payload);
+
     echo "start\n";
     echo shell_exec("git pull origin master 2>&1") . "\n";
     echo "finish\n";
