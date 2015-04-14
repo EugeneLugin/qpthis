5c7195bc56c082bde825db384f70c7fc24a6a799
Eugene Lugin <call.eugene@gmail.com>
2015-04-14 10:06:46 +0300

    Send SMS for 2 and 4 days delivered statuses

--- api/old/send_sms_list.php
+++ api/old/send_sms_list.php
@@ -24,8 +24,8 @@
 		$myXML 	.= "		<recipient>".$recipient."</recipient>";
 		$myXML 	.=  "</message>";
 		$myXML 	.= "</request>";
-echo $myXML;
-		$ch = curl_init();
+
+        $ch = curl_init();
 		curl_setopt($ch, CURLOPT_USERPWD , $smsfly_user.':'.$smsfly_password);
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
 		curl_setopt($ch, CURLOPT_POST, 1);
@@ -114,9 +114,6 @@ echo $myXML;
 	catch(PDOException $ex){ die("Невозможно выполнить запрос: " . $ex->getMessage()); }
 	$orders = $stmt->fetchAll();
 
-	echo '<h2>Список SMS для отправки</h2>';
-	echo '<table border=1 cellpadding=4 style="border: 1px solid black; border-collapse: collapse;">';
-	echo '<tr><th>ФИО клиента</td><td>Телефон</td><td>Длина</td><td>Текст СМС</td><td>Альфа</td></tr>';
 	foreach ($orders as $ord) {
 		$np_answer = json_decode($ord['newpost_answer'], true);
 		if ($np_answer['arrival_date']) {
@@ -158,49 +155,10 @@ echo $myXML;
 					$short_adr = trim($short_adr[2]);
 					$sms_text = $ord['fio'].'! Ваш заказ '.$ord['short_name'].' ТТН '.$ord['newpost_id'].' прибудет '.$short_date[1].' на склад '.$whsn.$short_adr;
                     $sms_text = $sms_text.substr(0,210);
-					$length = strlen(utf8_decode($sms_text));
-					echo '<tr><td>'.$ord['fio'].'</td><td>'.$ord['phone'].'</td><td class="sms_len" id="sms_len'.$ord['id'].'" align=center style="'.($length < 0 ? 'background-color: red' : '').'">'.$length.'</td><td>';
 
-					?>
-					<?php if (isset($_GET['seller_id'])) { ?>
-						<input type='hidden' name='seller_id' value='<?php echo $_GET['seller_id']?>'>
-					<?php }
-						if (isset($_GET['item_id'])) { ?>
-						<input type='hidden' name='item_id' value='<?php echo $_GET['item_id']?>'>
-					<?php } ?>
-					<?php if (isset($_GET['status_id'])) { ?>
-						<input type='hidden' name='status_id' value='<?php echo $_GET['status_id']?>'>
-					<?php } ?>
-					<?php if (isset($_GET['order_date'])) { ?>
-						<input type='hidden' name='order_date' value='<?php echo $_GET['order_date']?>'>
-					<?php } ?>
-					<?php if (isset($_GET['order_date_end'])) { ?>
-						<input type='hidden' name='order_date_end' value='<?php echo $_GET['order_date_end']?>'>
-					<?php } ?>
-					<input name="sms[<?php echo $ord['id'];?>][txt]" onkeyup="$('#sms_len<?php echo $ord['id']; ?>').html($(this).val().length).css('background-color',(($(this).val().length > 210) ? 'red':'white'));" style='width:500px' type='text' value='<?php echo $sms_text; ?>'>
-					<?php echo '</td><td>'.$ord['alphaname'].'</td></tr>';
-
-					if (isset($_GET['set'])) {
-							$sd = new DateTime();
-							$sd = $sd->format('Y-m-d H:i:s');
-
-							$query1 = "INSERT INTO flysms (status,date,campaignID,recipient,order_id,text) VALUES (:status,:date,:campaignID,:recipient,:order_id,:text)";
-
-							$query_params1 = array(
-								':campaignID' => 1,
-								':recipient' => "38".str_replace(array('(',')','-'), "", $ord['phone']),
-								':order_id' => $ord['id'],
-								':status' => 'DELIVERED',
-								':date' => $sd,
-                                ':text' => $sms_text
-							);
-
-							try{
-								$stmt1 = $db->prepare($query1);
-								$result = $stmt1->execute($query_params1);
-							}
-							catch(PDOException $ex){ die("Невозможно выполнить запрос: " . $ex->getMessage()); }
-					}
+                    echo $ord['phone'].' #';
+                    echo $ord['id'].': ';
+                    echo $sms_text.'<br>';
 
 					if (isset($_GET['view'])) {
 						continue;
@@ -249,12 +207,10 @@ echo $myXML;
 	?>
 	</table>
 <?php
-exit();
-
 	$query = "
-		SELECT
+        SELECT
+		    'HochuKupit' as alphaname,
 			orders.id as id,
-			COALESCE(item.short_name, 'заказ') as item,
 			orders.item_id as item_id,
 			orders.item_price as item_price,
 			orders.item_params as item_params,
@@ -267,8 +223,9 @@ exit();
 			orders.email as email,
 			orders.newpost_id as newpost_id,
 			orders.newpost_answer as newpost_answer,
-			orders.status_step2 as status_step2
-		   FROM orders LEFT OUTER JOIN item ON orders.item_id = item.uuid
+			orders.status_step2 as status_step2,
+			item.short_name as short_name
+		   FROM orders JOIN item ON orders.item_id = item.uuid
 			WHERE
 				HOUR(NOW()) >= 0 AND
 				HOUR(NOW()) <= 23 AND
@@ -289,7 +246,7 @@ exit();
 		$query_sms = "SELECT
 			date,
 			status
-		FROM flysms WHERE type = 2 AND order_id = :order_id AND status = 'DELIVERED'";
+		FROM flysms WHERE type = 2 AND order_id = :order_id";// AND status = 'DELIVERED'";
 		$query_params = array(':order_id' => $ord['id']);
 
 		try{
@@ -301,7 +258,7 @@ exit();
 		$delivered = $stmt->fetch();
 
 		if (!$delivered) {
-			$query_sms = "SELECT
+			/*$query_sms = "SELECT
 				date,
 				status
 			FROM flysms WHERE type=2 AND order_id = :order_id AND HOUR(TIMEDIFF(NOW(), date)) < 24";
@@ -313,53 +270,46 @@ exit();
 			}
 			catch(PDOException $ex){ die("Невозможно выполнить запрос: " . $ex->getMessage()); }
 
-			$this_day_sent = $stmt->fetch();
+			$this_day_sent = $stmt->fetch();*/
+			$this_day_sent = false;
 
 			if (!$this_day_sent) {
-				$short_item = explode(' ',$ord['item']);
-				$sms_text = 'Успейте забрать '.($ord['status_step2']==223 ? 'сегодня ':'').$ord['item'].' Накл.'.$ord['newpost_id'];
-				echo $ord['phone'].'<br>';
-				echo $ord['id'].'<br>';
+                $np_answer = json_decode($ord['newpost_answer'], true);
+                $rexp_adr = '/[^\d]*(\d*).*?:([^\(<]*)/';
+                preg_match($rexp_adr, $np_answer['address'], $short_adr);
+                $whsn = ($short_adr[1] && !empty($short_adr[1])) ? $short_adr[1].':' : '';
+                $short_adr = trim($short_adr[2]);
+
+                $sms_text = 'ПРИБЫЛ. '.($ord['status_step2']==223 ? 'Получите сегодня пожалуйста! ':'Приглашаем получить! ').$ord['short_name'].' ТТН '.$ord['newpost_id'].' склад '.$whsn.$short_adr;;
+				echo $ord['phone'].' #';
+				echo $ord['id'].': ';
 				echo $sms_text.'<br>';
 
-				if ($_GET['view']) {
+				if (isset($_GET['view'])) {
 					continue;
 				}
 
-				send_sms($sms_text,"38".str_replace(array('(',')','-'), "", $ord['phone']),$ord['id'],(($ord['alphaname'] and $ord['alphaname'] != '') ? $ord['alphaname'] : null),2);
+                send_sms($sms_text,"38".str_replace(array('(',')','-'), "", $ord['phone']),$ord['id'],(($ord['alphaname'] and $ord['alphaname'] != '') ? $ord['alphaname'] : null),$ord['status_step2']==223 ? 3: 2);
 
-				$query = "INSERT INTO debug (msg) VALUES (:msg)";
+                $query = "INSERT INTO orders_audit(date, order_id, user_id, activity, details) VALUES
+									(	NOW(),
+										:order_id,
+										:user_id,
+										:activity,
+										:details		)";
 
-				$query_params = array(
-					':msg' => $ord['phone'].': '.$sms_text
-				);
+                $query_params = array(
+                    ':details' => $sms_text,
+                    ':activity' => 'Отправлено SMS: '.$sms_text,
+                    ':user_id' => 1,
+                    ':order_id' => $ord['id']
+                );
 
-				try{
-					$stmt = $db->prepare($query);
-					$result = $stmt->execute($query_params);
-				}
-				catch(PDOException $ex){ die("Невозможно выполнить запрос: " . $ex->getMessage()); }
-
-
-				$query = "INSERT INTO orders_audit(date, order_id, user_id, activity, details) VALUES
-							(	NOW(),
-								:order_id,
-								:user_id,
-								:activity,
-								:details		)";
-
-				$query_params = array(
-					':details' => $sms_text,
-					':activity' => 'Отслеживание доставки: отправлено SMS',
-					':user_id' => 1,
-					':order_id' => $ord['id']
-				);
-
-				try{
-					$stmt = $db->prepare($query);
-					$result = $stmt->execute($query_params);
-				}
-				catch(PDOException $ex){ die("Невозможно выполнить запрос: " . $ex->getMessage()); }
+                try{
+                    $stmt = $db->prepare($query);
+                    $result = $stmt->execute($query_params);
+                }
+                catch(PDOException $ex){ die("Невозможно выполнить запрос: " . $ex->getMessage()); }
 			}
 		}
 	}
