419b12e7eb1c40c13372b154060137407904ccb0
Eugene Lugin <call.eugene@gmail.com>
2015-04-27 16:39:12 +0300

    Favicon XK

--- api/old/do_order.php
+++ api/old/do_order.php
@@ -79,7 +79,8 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<link rel="shortcut icon" type="image/ico" href="favicon.ico" />
+<link rel="shortcut icon" href="Logo99.png" type="image/x-icon">
+<link rel="icon" href="Logo99.png" type="image/jpg">
 <title>Ваш заказ принят!</title>
     <style type="text/css">
         #posts {
