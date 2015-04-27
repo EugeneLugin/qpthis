c4ba0ba061e40ad99bab888f04d1ccf5cb64ef36
Eugene Lugin <call.eugene@gmail.com>
2015-04-27 16:34:14 +0300

    Favicon

--- api/old/do_order.php
+++ api/old/do_order.php
@@ -79,6 +79,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="shortcut icon" type="image/ico" href="favicon.ico" />
 <title>Ваш заказ принят!</title>
     <style type="text/css">
         #posts {
